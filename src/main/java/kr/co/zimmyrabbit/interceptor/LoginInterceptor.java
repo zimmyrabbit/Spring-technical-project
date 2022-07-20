bc5cf26db79fc4b2f3023753342c183f26759eec
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2022-07-20 22:25:28 +0900

    인터셉터 생성

---
+++ src/main/java/kr/co/zimmyrabbit/interceptor/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package kr.co.zimmyrabbit.interceptor;
+
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		HashMap<String,Object> sessionInfo = (HashMap<String,Object>) request.getSession().getAttribute("userSession");
+		
+		if(sessionInfo != null) {
+			return true;
+		} else {
+			
+			response.setContentType("text/html; charset=UTF-8");
+
+            PrintWriter out = response.getWriter();
+
+			out.print("<script>location.replace('/login/loginform'); </script>");
+			
+			out.flush();
+			out.close();
+
+			return false;
+		}
+	}
+
+}
